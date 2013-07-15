3065d43ec7256ef090bacddffe126e4c6a825ed9
simongenest <simon.genest@me.com>
2013-07-15 13:46:56 -0400

    Update rem.js
    Modified to remove all keyframes.

--- js/rem.js
+++ js/rem.js
@@ -159,7 +159,7 @@
     // Remove queries.
     removeMediaQueries = function(css) {
         if (!mediaQuery()) {
-            removeBrowserSpecificQueries();
+            removeKeyframes();
             while (css.match(/@media/) !== null) { // If CSS syntax is correct there should always be a "@media" str matching a "}\s*}" string
                 var start = css.match(/@media/).index,
                     end = css.match(/\}\s*\}/);
@@ -171,16 +171,16 @@
     },
     
     // removes browser specific queries
-    removeBrowserSpecificQueries = function(css) {
+    removeKeyframes = function(css) {
     	if (mediaQuery()) {
     		return css;
     	}
-	while (css.match(/@-/) !== null) { 
-           var start = css.match(/@-/).index,
+		while (css.match(/@\S*keyframes/) !== null) { 
+           var start = css.match(/@\S*keyframes/).index,
            end = css.match(/\}\s*\}/);
            css = css.substring(0, start) + css.substring(end.index + end[0].length);
-	}	
-	return css;
+		}	
+		return css;
     }
 
 
